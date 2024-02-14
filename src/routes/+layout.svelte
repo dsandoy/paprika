e8ddf95eb2c2a668934af32aabeb02d8ca7163ed
daniev <danievprogramming@gmail.com>
2024-02-14 20:35:19 +0100

    Add tailwind css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import '../app.css'; // import the tailwind stuff
+</script>
+
+<slot />
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,ts,js,svelte}"],
+  theme: {
+    colors: {
+      "green": "#89A492",
+      "grey": {
+        100: "e3e3e3",
+        200: "cfcfcf",
+        300: "bfbfbf",
+      },
+      "white": "#fdffe",
+      "red": "#dc8491",
+      "blue": "#6090af",
+      "p-green": "#529873",
+      "p-yellow": "#f9d639",
+      "p-purple": "#e8b5fd",
+      "p-blue": "#39d6f9",
+    },
+    borderRadius: {
+      "none": "0",
+      DEFAULT: "5px",
+      "lg": "84px",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
