dfb1bba798312a22782b91be1aa6b2c19fd51cd4
daniev <danievprogramming@gmail.com>
2024-02-18 21:06:16 +0100

    Add the top bar
    Or the start of a top bar at least

---
+++ src/routes/(authed)/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	// the top bar is inserted here...
+</script>
+
+<header class="w-svw h-16 flex bg-green justify-between px-5 items-center text-white">
+	<div class="text-white">Menu</div>
+	<div><img class="w-16 h-16" src="paprika_logos/green.png" alt="Logo img" /></div>
+	<div>user info</div>
+</header>
+
+<slot />
