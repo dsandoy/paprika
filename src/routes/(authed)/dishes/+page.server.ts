1ab9ad7f33e8137253ab64421f2281b5981acdf5
daniev <danievprogramming@gmail.com>
2024-02-21 21:29:16 +0100

    Run formatter

--- src/routes/(authed)/dishes/+page.server.ts
+++ src/routes/(authed)/dishes/+page.server.ts
@@ -1,16 +1,16 @@
 import { prisma } from '$lib/prisma.js';
 import { error } from '@sveltejs/kit';
 
-export const load = async (event ) => {
+export const load = async (event) => {
 	const session = await event.locals.auth();
 	if (!session?.user) {
 		error(401, 'unauthorized');
 	}
-	if (!session.user.email)
-	{
+	if (!session.user.email) {
 		error(401, 'this user does not have an email?!');
 	}
-	const data = await prisma.dish.findMany({
+	const data = await prisma.dish
+		.findMany({
 			where: {
 				user: session.user.email
 			},
@@ -23,6 +23,6 @@ export const load = async (event ) => {
 			error(500, 'something went wrong when collecting data: ' + err);
 		});
 	return {
-			response: data
-		};
+		response: data
+	};
 };
