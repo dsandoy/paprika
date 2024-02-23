f262c8b4c88441f882d398dfb9156ad90a04ab97
daniev <danievprogramming@gmail.com>
2024-02-23 20:19:23 +0100

    Add form submittion validation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,6 @@
+import { get } from 'svelte/store';
+import { dishes } from './stores';
+
 /** Display the date in the date number and month only. Ex: 12. feb,
  * if no datevalue, return emptyMessage, else return empty string
  */
@@ -9,7 +12,39 @@ export function showDate(date: Date | null, emptyMessage = '') {
 		return '';
 	}
 	const dateStr = date.toDateString();
-	// remove weekday and year:
+	// remove month and year:
 	const dateArr = dateStr.split(' ');
 	return dateArr[0] + ' ' + dateArr[2];
+}
+
+export const URL_INVALID = -3;
+
+export const DATABASE_ERROR = -8;
+/** Validate that the inputed value is a url */
+export function validateURL(url: string) {
+	const pattern = new RegExp(
+		'^(https?:\\/\\/|http?:\\/\\/)?' + // protocol
+			'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+			'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+			'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+			'(\\#[-a-z\\d_]*)?$',
+		'i'
+	); // fragment locator
+	return pattern.test(url);
+}
+
+/** error code for name empty */
+export const NAME_EMPTY = -1;
+/** error code for naem already in use */
+export const NAME_ALREADY_IN_USE = -2;
+/** Check that the name is not empty or already in use */
+export function validateName(name: string, names: string[] | null = null) {
+	if (name.length === 0) return NAME_EMPTY;
+
+	if (names == null) names = get(dishes).map((dish) => dish.name.toLowerCase());
+	if (names.includes(name.toLowerCase())) {
+		return NAME_ALREADY_IN_USE;
+	}
+	return 0;
 }
--- src/routes/(authed)/dishes/add/+page.server.ts
+++ src/routes/(authed)/dishes/add/+page.server.ts
@@ -1,9 +1,67 @@
+import { get } from 'svelte/store';
+import {
+	validateName,
+	validateURL,
+	DATABASE_ERROR,
+	NAME_ALREADY_IN_USE,
+	NAME_EMPTY,
+	URL_INVALID
+} from '$lib/utils';
+import { page } from '$app/stores';
+import { prisma } from '$lib/prisma';
 
-export function actions() {
-    // validate user and include it to submittions..
-    // validate that the name is not empty and not already in use
-    // validate url..
-    // handle image..
-    return {
-    };
-}
+const USER_INVALID = -10;
+export const actions = {
+	default: async ({ request }) => {
+		const user = get(page).data.session?.user?.email;
+		if (!user) {
+			return {
+				error: USER_INVALID,
+				message: 'Du må være innlogget for å legge til en matrett'
+			};
+		}
+		const data = request.formData();
+		const name = data.get('name');
+		const result = validateName(name);
+		if (result === NAME_EMPTY) {
+			return {
+				error: NAME_EMPTY,
+				message: 'Du kan ikke ha et tomt navn'
+			};
+		}
+		if (result === NAME_ALREADY_IN_USE) {
+			return {
+				error: NAME_ALREADY_IN_USE,
+				message: 'Du har allerede en matrett med det navnet, kall denne noe annet'
+			};
+		}
+
+		const url = data.get('url');
+		const result2 = validateURL(url);
+		if (!result2) {
+			return {
+				error: URL_INVALID,
+				message: 'Oppgi en gyldig URL'
+			};
+		}
+		const dish = {
+			name: name,
+			url: url,
+			user: user
+		};
+		await prisma.dish
+			.create({
+				data: dish
+			})
+			.catch((error) => {
+				return {
+					error: DATABASE_ERROR,
+					message: 'Klarte ikke å opprette matretten i databasen.. Noe gikk galt...' + error
+				};
+			});
+
+		return {
+			success: true
+		};
+	}
+};
