5f25316a20cafb8fc6347e601824bb89ebc5c9a5
daniev <danievprogramming@gmail.com>
2024-03-05 19:54:34 +0100

    Make top bar more mobile friendly

--- src/lib/components/user/ProfileDropdown.svelte
+++ src/lib/components/user/ProfileDropdown.svelte
@@ -7,13 +7,13 @@
 
 <Dropdown
 	classNamesButton="flex items-center gap-3 cursor-pointer relative"
-	classNamesContent="w-48 absolute bg-gray-200 z-10 text-black p-5 right-5 top-14 rounded-sm "
+	classNamesContent="w-48 absolute bg-gray-100 border-[1px] border-green z-10 text-black p-5 right-5 top-14 rounded "
 >
 	<span slot="button">
 		{#if $page?.data?.session?.user?.image}
-			<img class="rounded-lg h-11" src={$page.data.session.user.image} alt="profile" />
+			<img class="rounded-lg h-10 lg:h-11" src={$page.data.session.user.image} alt="profile" />
 		{:else}
-			<img class="rounded-lg h-11" src="google-logo.png" alt="profile" />
+			<img class="rounded-lg h-10 lg:h-11" src="google-logo.png" alt="profile" />
 		{/if}
 	</span>
 	<div slot="content" class="flex flex-col gap-4">
