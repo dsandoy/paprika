92e0a59a9a88256588eaf4d8b8bc514334ba806d
daniev <danievprogramming@gmail.com>
2024-03-08 19:22:39 +0100

    Add some icons

--- src/lib/components/SecondaryButton.svelte
+++ src/lib/components/SecondaryButton.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <button
-	class=" h-11 text-lg rounded-lg px-5 cursor-pointer border-[1px] border-green bg-gradient-to-r from-gray-100 to-gray-100 via-white hover:from-green hover:to-green hover:text-white {classNames}"
+	class=" h-11 text-lg flex justify-center items-center rounded-lg px-5 cursor-pointer border-[1px] border-green bg-gradient-to-r from-gray-100 to-gray-100 via-white hover:from-green hover:to-green hover:text-white {classNames}"
 	{type}
 	{formaction}
 	on:click
