43b3c03ab946513ee9d0f1c53c2cd18a7b558267
daniev <danievprogramming@gmail.com>
2024-03-09 20:31:51 +0100

    Try to adjust top scroll again

--- src/routes/(authed)/dashboard/+page.svelte
+++ src/routes/(authed)/dashboard/+page.svelte
@@ -8,6 +8,5 @@
 		Denne siden er ikke klar ennå, men kommer nok snart... Da vil den se ca sånn ut!
 	</p>
 
-	<img class="w-full lg:w-[50%]" src="/peaks/dashbord.png" alt="dashbord-preview" />
 	<BottomCircles></BottomCircles>
 </section>
