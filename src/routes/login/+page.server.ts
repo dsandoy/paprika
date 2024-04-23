85cab87c370204c171960748cb6141f74c1f2243
dsandoy <danievprogramming@gmail.com>
2024-04-23 20:59:52 +0200

    Remove google login as it is too inconsistent

--- src/routes/login/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { auth } from '$lib/Firebase';
-import { redirect } from '@sveltejs/kit';
-export async function load() {
-	if (auth.currentUser !== null) {
-		throw redirect(307, '/');
-	}
-	return {};
-}
