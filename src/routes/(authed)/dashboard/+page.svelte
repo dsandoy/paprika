1d34592a85211a4d1c992c0604e10a01a3dd57b2
dsandoy <danievprogramming@gmail.com>
2024-05-01 10:56:10 +0200

    Add hover style for dashboard

--- src/routes/(authed)/dashboard/+page.svelte
+++ src/routes/(authed)/dashboard/+page.svelte
@@ -83,7 +83,7 @@
 		<div class="flex flex-row gap-5 items-center justify-center pt-4">
 			{#each navigations as nav}
 				<a
-					class="text-sm p-2 pl-4 pr-4 gap-2 rounded flex flex-col items-center justify-center bg-white"
+					class="text-sm p-2 pl-4 pr-4 gap-2 rounded flex flex-col items-center justify-center bg-white hover:bg-grey-100"
 					href={nav.url}
 				>
 					<Icons iconName={nav.icon} height="2rem" />
@@ -98,7 +98,9 @@
 				<h1 class="text-xl lg:text-2xl lg:text-center">Lag Middag</h1>
 				<div class="flex flex-col lg:flex-row gap-2 lg:gap-8 lg:items-center lg:justify-center">
 					<a href={getUrl($closePlans[0])}>
-						<button class="flex flex-row gap-2 p-2 items-center w-full lg:w-64 bg-white rounded">
+						<button
+							class="flex flex-row gap-2 p-2 items-center w-full lg:w-64 bg-white hover:bg-grey-100 rounded"
+						>
 							<DishImage classNames="h-11 w-11" imagesrc={getimageURL($closePlans[0])} />
 							<p class="text-xs text-slate-400">I går</p>
 							<p class="text-sm text-slate-400">{getName($closePlans[0])}</p>
@@ -106,7 +108,7 @@
 					</a>
 					<a href={getUrl($closePlans[1])}>
 						<button
-							class="flex flex-row gap-3 p-2 items-center rounded w-full lg:w-64 bg-nice-blue/40"
+							class="flex flex-row gap-3 p-2 items-center rounded w-full lg:w-64 bg-nice-blue/40 hover:bg-nice-blue/60"
 						>
 							<DishImage classNames="h-11 w-11" imagesrc={getimageURL($closePlans[1])} />
 							<p class="text-sm text-nice-blue/70 font-semibold">I dag</p>
@@ -114,7 +116,9 @@
 						</button>
 					</a>
 					<a href={getUrl($closePlans[2])}>
-						<button class="flex flex-row gap-2 p-2 items-center w-full lg:w-64 bg-white rounded">
+						<button
+							class="flex flex-row gap-2 p-2 items-center w-full lg:w-64 bg-white hover:bg-grey-100 rounded"
+						>
 							<DishImage classNames="h-11 w-11" imagesrc={getimageURL($closePlans[2])} />
 							<p class="text-xs text-slate-400">I morgen</p>
 							<p class="text-sm text-slate-400">{getName($closePlans[2])}</p>
