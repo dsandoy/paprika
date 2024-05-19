ce002a192eee88a0642266dff86d3754a7ad5e57
dsandoy <danievprogramming@gmail.com>
2024-05-19 16:08:25 +0200

    Try to fix auth issues

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,12 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/sveltekit/providers/google';
-import { GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private';
+import { AUTH_SECRET, GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private';
 
-export const { handle, signIn, signOut } = SvelteKitAuth({
-	providers: [Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })]
+export const { handle, signIn, signOut } = SvelteKitAuth(async () => {
+	const authOptions = {
+		providers: [Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })],
+		secret: AUTH_SECRET,
+		trustHost: true
+	};
+	return authOptions;
 });
