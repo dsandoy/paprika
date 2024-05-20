1b1717b341d480338e04049e2b54c1ab36a56012
dsandoy <danievprogramming@gmail.com>
2024-05-20 20:17:25 +0200

    Fix ingredient edit bug

--- src/routes/(authed)/dishes/edit/[slug]/+page.server.ts
+++ src/routes/(authed)/dishes/edit/[slug]/+page.server.ts
@@ -1,5 +1,5 @@
 import { DishQueries } from '$lib/server/queries.js';
-import type { UpdateDish, UpdateImage, UpdateIngredient } from '$lib/types.js';
+import type { CreateIngredient, UpdateDish, UpdateImage, UpdateIngredient } from '$lib/types.js';
 import { DishValidator, type ValidationResult } from '$lib/utils';
 import { error, redirect } from '@sveltejs/kit';
 
@@ -21,14 +21,22 @@ export const actions = {
 		const data = await request.formData();
 		let v: ValidationResult = { is_valid: true, message: '' };
 
-		// TODO: Ensure that ingredients dont have IDs...
-
 		const dish: UpdateDish = {
 			id: parseInt(data.get('id') as string),
 			name: data.get('name') as string,
 			url: data.get('url') as string,
 			ingredients: JSON.parse(data.get('ingredients') as string) as UpdateIngredient[]
 		};
+
+		if (dish.ingredients && dish.ingredients.length > 0) {
+			const processedIngredients: CreateIngredient[] = [];
+			for (const ingredient of dish.ingredients) {
+				processedIngredients.push({
+					value: ingredient.value
+				});
+			}
+			dish.ingredients = processedIngredients;
+		}
 
 		const image = data.get('image') as File;
 		if (image.size != 0) v = DishValidator.validateImage(image);
@@ -72,9 +80,9 @@ export const actions = {
 			await DishQueries.update(dish);
 		} catch (error) {
 			console.error(error);
-			let message = 'Database Error';
+			let message = 'Database Error! Vennligst refresh siden (ctrl + r)';
 			if (error instanceof Error || (typeof error === 'object' && error?.constructor === Error)) {
-				message = 'Database Error: ' + error.name;
+				message = 'Database Error: ' + error.name + 'Vennligst refresh siden (ctrl + r)';
 			}
 			return {
 				data: dish,
